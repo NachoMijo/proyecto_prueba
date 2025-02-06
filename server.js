b527ea4da71489213a4551250e860efd14832aff
NachoMijo <nacho.barbaquezada@gmail.com>
2025-02-06 23:05:33 +0100

    Server Express

---
+++ server.js
@@ -0,0 +1,18 @@
+"use strict"
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+
+const { PORT } = process.env;
+
+app.get("/", function (req, res) {
+  res.send("Nachete");   
+});
+
+app.listen(PORT, () => {
+  console.log(`http://127.0.0.1:${PORT}`);
+});
+
